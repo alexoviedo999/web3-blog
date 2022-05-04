ab66c0a38aa6012f833d6b9c1224c122b271feb5
Alejandro Oviedo <alexoviedo999@gmail.com>
2022-05-03 22:33:52 -0500

    adding context for state. starting UI coding and adding some assets

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,54 @@
 import '../styles/globals.css'
+import { useState } from 'react'
+import Link from 'next/link'
+import { css } from '@emotion/css'
+import { ethers } from 'ethers'
+import Web3Modal from 'web3modal'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import { AccountContext } from '../context.js'
+import { ownerAddress } from '../config'
+import 'easymde/dist/easymde.min.css'
+
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  /* create local state to save account information after signin */
+  const [account, setAccount] = useState(null)
 
-export default MyApp
+  /* web3Modal configuration for enabling wallet access */
+  async function getWeb3Modal() {
+    const web3Modal = new Web3Modal({
+      cacheProvider: false,
+      providerOptions: {
+        walletconnect: {
+          package: WalletConnectProvider,
+          options: { 
+            infuraId: INFURA_PROJECT_ID
+          },
+        },
+      },
+    })
+    return web3Modal
+  }
+
+    /* the connect function uses web3 modal to connect to the user's wallet */
+    async function connect() {
+      try {
+        const web3Modal = await getWeb3Modal()
+        const connection = await web3Modal.connect()
+        const provider = new ethers.providers.Web3Provider(connection)
+        const accounts = await provider.listAccounts()
+        setAccount(accounts[0])
+      } catch (err) {
+        console.log('error:', err)
+      }
+    }
+
+    return (
+      <div>
+        <nav className={nav}>
+
+        </nav>
+      </div>
+    )
+}
